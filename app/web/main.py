c43246fd060a45d62d6ce5f6c93e766f8042e0b0
iGolden <136895062@qq.com>
2020-04-19 11:11:15 +0800

    添加模版文件

---
+++ app/web/main.py
@@ -0,0 +1,13 @@
+from app.web.__inint__ import web
+
+__author__ = '七月'
+
+
+@web.route('/')
+def index():
+    pass
+
+
+@web.route('/personal')
+def personal_center():
+    pass
--- app/web/user.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/4/2 下午5:14
-# @Author  : iGolden
-# @Software: PyCharm
